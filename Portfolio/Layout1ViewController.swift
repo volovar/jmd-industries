07962ff4e0e100401f6f6ca526b02921238bc467
dclatzen <devon@studyblue.com>
2016-11-28 17:07:56 -0800

    Continued work on the project page layout.

--- Portfolio/Layout1ViewController.swift
+++ Portfolio/Layout1ViewController.swift
@@ -11,11 +11,17 @@ import AFNetworking
 
 class Layout1ViewController: UIViewController {
     
-    @IBOutlet weak var projectDescription: UITextView!
+    
     @IBOutlet weak var imageOne: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
     
     // Text
+    
+    @IBOutlet weak var projectDescription: UITextView!
+    @IBOutlet weak var researchText: UITextView!
+    @IBOutlet weak var theProblemText: UITextView!
+    @IBOutlet weak var theSolutionText: UITextView!
+    @IBOutlet weak var resultsText: UITextView!
     
     var bodyTextStyle: NSMutableParagraphStyle!
     var bodyTextAttributes: [String : Any]!
@@ -49,6 +55,21 @@ class Layout1ViewController: UIViewController {
 
         styleBodyParagraphs(referenceTextView: projectDescription, bodyText: projectDescription.text)
         
+        theProblemText.text = "With the popularity of apps like Meerkat and Periscope, we noticed that people wanted to share events in real-time, but 'social network fatigue' would set in when they opened multiple apps to share their experiences. It made sense for the world's largest social network to be a reliable hub for real-time communication."
+        
+        styleBodyParagraphs(referenceTextView: theProblemText, bodyText: theProblemText.text)
+        
+        researchText.text = "Our team used two primary research methods to understand the user's needs. Our primary method was field observation. We recorded user behaviors at the moment when they utilized curent market offerings. We confirmed our hypothesis that users are drawn to live sharing apps primarily due to the immediate feedback they receive. A notable drawback in the current market offering was what we termed 'social network fatigue,' caused by switching between different sharing apps. Over 80% of target users shared live once, while only 37% shared live twice, opting instead to record experiences in less ephemeral ways. Our second method involved interviews with the target user base. We obtained valuable insight into use-cases we had previously not considered, such as sharing live as a means of direct communication."
+        
+        styleBodyParagraphs(referenceTextView: researchText, bodyText: researchText.text)
+        
+        theSolutionText.text = "Our solution at Facebook was to give users the ability to share in real-time, using a top-level affordance in their mobile app. Users would be able share traditional photos and text, while sharing live, through one seamless experience."
+        
+        styleBodyParagraphs(referenceTextView: theSolutionText, bodyText: theSolutionText.text)
+        
+        resultsText.text = "Early adoption was strong, with 3% of users exploring the feature within the first week of launch. At the close of the first month, nearly 47% of users globally were sharing live on a regular basis."
+        
+        styleBodyParagraphs(referenceTextView: resultsText, bodyText: resultsText.text)
         
     } // end viewDidLoad
     
