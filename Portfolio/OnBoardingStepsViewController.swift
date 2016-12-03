97d7353951ae21a92f205b6e8bf2e3396bf1d7b8
JeanMenezes <jottamenezes@gmail.com>
2016-12-03 13:07:30 -0800

    finished OnBoarding

--- Portfolio/OnBoardingStepsViewController.swift
+++ Portfolio/OnBoardingStepsViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class OnBoardingStepsViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var onBoardingMask: UIImageView!
     @IBOutlet weak var onBoardingPageControl: UIPageControl!
     @IBOutlet weak var scrollVIew: UIScrollView!
+    
+    @IBOutlet weak var getStartedButton: UIButton!
+    @IBOutlet weak var sassFinal: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollVIew.delegate = self
         
-        scrollVIew.contentSize = CGSize(width: 1500, height: 667)
+        scrollVIew.contentSize = CGSize(width: 1875, height: 667)
         scrollVIew.frame.size = CGSize(width: 375, height: 667)
 
-//        carousselButton.alpha = 0
 
         // Do any additional setup after loading the view.
     }
@@ -33,15 +37,46 @@ class OnBoardingStepsViewController: UIViewController, UIScrollViewDelegate {
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let page: Int = Int(round(scrollView.contentOffset.x / 375))
-        
         onBoardingPageControl.currentPage = page
+
         
         if page == 3 {
-          
-            UIView.animate(withDuration: 0.7, animations: {
-              
-            })
             
+            self.getStartedButton.transform = CGAffineTransform.identity
+            self.sassFinal.transform = CGAffineTransform.identity
+            self.getStartedButton.alpha = 0
+            onBoardingMask.alpha = 1
+            
+            
+        }
+        if page == 4 {
+//             self.sassFinal.transform = self.sassFinal.transform.translatedBy(x: 0, y: 380)
+            
+            self.getStartedButton.alpha = 0
+            UIView.animate(withDuration: 1.75, animations: {
+                self.onBoardingMask.alpha = 0.2
+                
+            })  { (Bool) in
+                UIView.animate(withDuration: 0.5, delay: 0.3, animations: {
+//                    self.getStartedButton.transform = self.getStartedButton.transform.scaledBy(x: 1.2, y: 1.2)
+                    self.getStartedButton.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                self.getStartedButton.alpha = 1
+                })
+            }
+            
+            UIView.animate(withDuration: 1.75, delay: 0.2, usingSpringWithDamping: 1, initialSpringVelocity: 0.7, options: [], animations:{
+                self.sassFinal.transform = CGAffineTransform(translationX: 0, y: -380)
+                
+            })
+            { (Bool) in
+                UIView.animate(withDuration: 0.4, delay: 0, options: [.repeat, .autoreverse], animations:{
+                    self.sassFinal.transform = CGAffineTransform(translationX: 0, y: -375)
+
+                    
+                })
+   
+            }
+  
         }
     }
     
